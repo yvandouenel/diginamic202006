a5e164819edc487ee3b2a9a8860363044b6f0ce2
yvandouenel <y.douenel@coopernet.fr>
2020-06-17 11:57:43 +0200

    utilisation du file system fs avec des promesses.

--- node/index.js
+++ node/index.js
@@ -1,8 +1,11 @@
 const http = require("http");
+const fs = require("fs");
+const fsPromises = require("fs").promises;
+
 // destructuring
 //const {ResponseWriter} = require('./response-writer');
 // ou const {ResponseWriter} = require('./response-writer');
-const ResponseWriter = require('./response-writer');
+const ResponseWriter = require("./response-writer");
 http
   .createServer((req, res) => {
     const responseWriter = new ResponseWriter(res);
@@ -10,8 +13,26 @@ http
       responseWriter.hello();
     } else if (req.url === "/weather") {
       responseWriter.weather(34172);
-    }else {
+    } else if (req.url === "/weather.jpg") {
+      res.writeHead(200, { "Content-type": "image/jpg" });
+      fsPromises.readFile(__dirname + '/weather.jpg')
+        .then(data => res.end(data));
+      //read the image using fs and send the image content back in the response
+      // fs.readFile(__dirname + '/weather.jpg', function (err, content) {
+      //   if (err) {
+      //     res.writeHead(400, { "Content-type": "text/html" });
+      //     console.log(err);
+      //     res.end("No such image");
+      //   } else {
+      //     //specify the content type in the response will be an image
+      //     res.writeHead(200, { "Content-type": "image/jpg" });
+      //     res.end(content);
+      //   }
+      // });
+    } else if (req.url === "/") {
       responseWriter.index();
+    } else {
+      responseWriter.htmlError(404, "Page introuvable");
     }
   })
   .listen(8000);
--- node/response-writer.js
+++ node/response-writer.js
@@ -6,6 +6,7 @@ class ResponseWriter {
   }
   index() {
     this.htmlSuccess(`<h1>Hello Bob</h1>
+    <p><img src="/weather.jpg" alt=""/></p>
     <p><a href="./hello">Heure du serveur ?</a></p>
     <p><a href="./weather">Météo à Mtp</a></p>
     `);
@@ -29,9 +30,15 @@ class ResponseWriter {
     const headers = { Accept: "application/json" };
     https.get(weatherApiUrl, { headers }, (apiResponse) => {
       let responseData = "";
+
+      // on se branche sur les événements data ici : on reçoit des bouts de réponse
       apiResponse.on("data", (chunk) => {
-        responseData += chunk;
+        responseData += chunk; // renvoit un buffer (série d'octets)
+        // la concaténation appelle la méthode toString. Ici ça se passe bien
+        // parece qu'il n'y a pas de caractères spéciaux ou que c'est déjà dans l'UTF-8
+        console.log("chunk : " + chunk); 
       });
+      // on se branche sur les événements data ici : on a reçu toutes les données
       apiResponse.on("end", () => {
         const json = JSON.parse(responseData);
         const tableRows = json.forecast
@@ -58,6 +65,19 @@ class ResponseWriter {
       <title>Document</title>
     </head>
     <body>${bodyContent}</body>
+    </html>`);
+  }
+  htmlError(status, message) {
+    this.res.writeHead(status, { "Content-Type": "text/html" });
+    this.res.end(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+    </head>
+    <body>Erreur - Status : ${status} - Message : ${message}</body>
     </html>`);
   }
 }
