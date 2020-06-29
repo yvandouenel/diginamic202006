c68e5e72bcdae902521f0ca31bc8be85b2a0df81
yvandouenel <y.douenel@coopernet.fr>
2020-06-29 17:15:15 +0200

    post de reservation ok.

--- hotel/hotelfront/src/components/public/Home.js
+++ hotel/hotelfront/src/components/public/Home.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 import Header from '../common/Header';
+import FetchData from '../../services/FetchData';
 
 class Home extends Component {
-  state = {}
-  handleSubmit = (event) => {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+    this.fd = new FetchData();
+  }
+  handleSubmit = async (event) => {
+    console.log('Dans handleSubmit');
     event.preventDefault();
+
+    const payload_reservation = {
+      start: event.target.querySelector("#start-date").value,
+      end: event.target.querySelector("#end-date").value,
+      persons: event.target.querySelector("#nb-person").value,
+      category: event.target.querySelector("#category").value
+    }
+    console.log('request : ', payload_reservation);
+
+    // Test des entrées 
+
+    // POST
+    try {
+      const reservation = await this.fd.postReservation(payload_reservation);
+      console.log('reservation');
+    } catch (error) {
+      console.log('Erreur : ', error);
+    }
+
+
+
   }
   render() {
     return (
@@ -17,19 +46,19 @@ class Home extends Component {
               <form onSubmit={this.handleSubmit} className="form-group">
                 <label htmlFor="start-date">
                   Date d'arrivée :
-                  <input className="form-control" id="start-date" type="date" defaultValue={""} />
+                  <input required className="form-control" id="start-date" type="date" defaultValue={""} />
                 </label>
                 <label htmlFor="end-date">
                   Date de départ :
-                  <input className="form-control" id="end-date" type="date" defaultValue={""} />
+                  <input required className="form-control" id="end-date" type="date" defaultValue={""} />
                 </label>
                 <label htmlFor="nb-person">
                   Nombre de personnes :
-                  <input className="form-control" id="nb-person" type="number" min="1" max="3" />
+                  <input required className="form-control" id="nb-person" type="number" min="1" max="3" />
                 </label>
                 <label htmlFor="category">
                   Catégorie de chambre :
-                  <select id="category">
+                  <select required id="category">
                     <option value="1">1 - Chambre simple</option>
                     <option value="2">2 - Chambre double</option>
                     <option value="3">3 - Chambre double - lits séparés</option>
--- hotel/hotelfront/src/services/FetchData.js
+++ hotel/hotelfront/src/services/FetchData.js
@@ -1,29 +1,61 @@
 class FetchData {
-    constructor() {
-        this.url = 'http://localhost:8000/';
-        this.headers = {
-            "Content-Type": "application/json",
-            Authorization: "Basic " + btoa("admin:admin") // btoa = encodage en base 64
-        };
-        this.credentials = "same-origin";
-    }
-    getReservations = () => {
-        return fetch(`${this.url}admin/reservations`, {
-            credentials: this.credentials,
-            method: "GET",
-            headers: this.headers
-        }).then(function (response) {
-            if (response.status != 200) {
-                throw new Error("Erreur " + response.status);
-            }
-            return response.json();// teste si c'est bien du json
-        })
-            .then(function (data) {
-                console.log('data : ', data);// J'ai ma donnée au format json
-                return data;
-            });
+  constructor() {
+    this.url = 'http://localhost:8000/';
+    this.headers = {
+      "Content-Type": "application/json",
+      Authorization: "Basic " + btoa("admin:admin") // btoa = encodage en base 64
+    };
+    this.credentials = "same-origin";
+  }
+  getReservations = () => {
+    return fetch(`${this.url}admin/reservations`, {
+      credentials: this.credentials,
+      method: "GET",
+      headers: this.headers
+    }).then(function (response) {
+      if (response.status != 200) {
+        throw new Error("Erreur " + response.status);
+      }
+      return response.json();// teste si c'est bien du json
+    })
+      .then(function (data) {
+        console.log('data : ', data);// J'ai ma donnée au format json
+        return data;
+      });
 
-    }
+  }
+  postReservation = ({ start, end, persons, category }) => {
+    const query_string = `?start=${start}&end=${end}&persons=${persons}&category=${category}`;
+    console.log('query_string : ', query_string);
+    return fetch(`
+        ${this.url}booking/try-booking${query_string}`, {
+      method: "POST",
+      headers: this.headers,
+      body: JSON.stringify({
+        "customer": {
+          "firstName": "string",
+          "lastName": "string",
+          "phone": "string",
+          "email": "string",
+          "address": {
+            "street": "string",
+            "zipcode": "string",
+            "city": "string",
+            "country": "string"
+          }
+        }
+      })
+    }).then(function (response) {
+      if (response.status !== 201) {
+        throw new Error("Erreur " + response.status);
+      }
+      return response.json();// teste si c'est bien du json
+    })
+      .then(function (data) {
+        console.log('data : ', data);// J'ai ma donnée au format json
+        return data;
+      });
+  }
 }
 
 export default FetchData;
